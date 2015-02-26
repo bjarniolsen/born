43c8deed4c6c3e2fbaea1af1950f3d6b3455a193
bjarniolsen <bjo@dis-play.dk>
2015-02-26 23:56:19 +0100

    Started styling. Added images.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Billedkunst for børn | Sassi Bischoff</title>
+        <title>Billedkunst for børn v/Sassi Bischoff</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -27,8 +27,8 @@
 
         <div class="header-container">
             <header class="wrapper clearfix" role="banner">
-                <h1 class="title" role="heading" aria-level="1">Billedkunst for børn</h1>
-				<p>Og et slogan måske</p>
+                <h1 class="title" role="heading" aria-level="1">Billedkunst for børn v/Sassi Bischoff</h1>
+				<p class="subtitle">Kulturstationen i Vanløse, spejlsalen</p>
                 <!--<nav role="navigation">
                     <ul>
                         <li><a href="#">nav ul li a</a></li>
@@ -42,95 +42,135 @@
 		<main class="main-container" role="main">
             <div class="main wrapper clearfix">
 
-				<section aria-labelledby="section-heading-ws-1">
-                    <h2 id="section-heading-ws-1" role="heading" aria-level="2">Workshop 1</h2>
-                    <p><!--img src="static/img/_sassi-bischoff.jpg" alt="Sassi Bischoff" width="100" height="120" /-->Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
-				</section>
-				<section aria-labelledby="section-heading-ws-2">
-                    <h2 id="section-heading-ws-2" role="heading" aria-level="2">Workshop 2</h2>
-                    <p><!--img src="static/img/_sassi-bischoff.jpg" alt="Sassi Bischoff" width="100" height="120" /-->Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
-				</section>
-				<section aria-labelledby="section-heading-subscribe">
-                    <h2 id="section-heading-subscribe" role="heading" aria-level="2">Tilmelding</h2>
+				<div class="main-content">
 
-                    <form action="subscribe.php" name="workshops" id="workshop" method="post">
-						<input type="hidden" name="price" value="50"/>
-						<fieldset>
-							<legend class="visuallyhidden">Personlige oplysninger</legend>
-							<label>
-								<span>Email adresse</span>
-								<input type="text" name="email" placeholder="ditnavn@mail.dk" />
-							</label>
-							<label>
-								<span>Mobilnummer</span>
-								<input type="text" name="phone" placeholder="12345678" />
-							</label>
-						</fieldset>
+					<section aria-labelledby="section-heading-ws-1">
+						<p>Elsker dit barn at tegne og male? - Så er denne workshop lige sagen!</p>
+						<p>Vi skal arbejde med tusch og smukke akvarelfarver. Børnenes fantasi udfordres og de inspireres til at udfolde deres billedsprog. Hvert barn får minimum ét færdigt billede med hjem. Der kræves ingen forudsætninger, kun lyst til at tegne/male.<br/>Påklædning: Praktisk tøj som godt kan tåle at få lidt maling på.</p>
+						<p>Intro-pris: 225 kr. pr. deltager.</p>
+						<p>Bemærk! Prisen er inkl. alle materialer (lærred, pensler, mv.)<br/>Jeg foretrækker at børnene arbejder med materialer af høj kvalitet, både for at få en god oplevelse og et godt resultat</p>
+						<p></p>
+						<p></p>
+						<p></p>
+						<p></p>
+					</section>
 
-						<fieldset>
-							<legend class="visuallyhidden">Vælg workshop</legend>
-							<label>
-								<input type="checkbox" name="checkbox[0]" /> <!-- checked="checked"-->
-								<span>Vælg workshop 1</span>
-							</label>
-							<div class="form-group" id="0">
+					<section aria-labelledby="section-heading-ws-1">
+                    	<h2 id="section-heading-ws-1" role="heading" aria-level="2">Workshop 1</h2>
+						<p>For de 5-7 årige<br/>Søndag d. 22. marts 2015 kl. 10-13.00</p>
+					</section>
+
+					<section aria-labelledby="section-heading-ws-2">
+                    	<h2 id="section-heading-ws-2" role="heading" aria-level="2">Workshop 2</h2>
+						<p>For de 8-10 årige<br/>Søndag d. 22. marts 2015 kl. 14-17.00</p>
+					</section>
+
+					<section aria-labelledby="section-heading-hold-1">
+                    	<h2 id="section-heading-hold-1" role="heading" aria-level="2">Hold 1</h2>
+						<p>
+							Billedkunst for børn 6-10 år 4 onsdage i april: d. 8. 15. 22. og 29. april kl. 15.00-17.00.<br/>
+							Vi skal arbejde med enkelte teknikker, tegning, akvarel og collage. Børnene inspireres til at finde motiver og lege med farvesammensætning. Der kræves ingen forudsætninger, kun lyst til at tegne/male.
+						</p>
+						<blockquote>
+							Påklædning: Praktisk tøj som godt kan tåle at få lidt maling på.<br/>
+							Intro-pris: 525 kr pr. deltager.<br/>
+							Bemærk! Prisen er inkl. alle materialer.<br/>
+						</blockquote>
+						<p>
+							Tilmelding kan ske over facebook, hjemmeside, mail eller telefon senest d.???<br/>
+							Min. 6 deltagere på hvert hold max. 12.<br/>
+							Betaling kan ske over mobilpay eller via kontooverførsel.<br/>
+							Ved tilmelding på hjemmesiden modtager du en mail med oplysninger omkring betaling.<br/>
+							Mærk din indbetaling med telefonnummer eller mailadresse.<br/>
+							Når din betaling er registreret, er din tilmelding gennemført.<br/>
+							Du modtager en bekræftelse på mail eller sms.<br/>
+							I tilfælde af, at holdet ikke kan oprettes ved for få tilmeldte, refunderes din indbetaling.<br/>
+						</p>
+					</section>
+
+					<section aria-labelledby="section-heading-subscribe">
+                    	<h2 id="section-heading-subscribe" role="heading" aria-level="2">Tilmelding</h2>
+
+                    	<form action="subscribe.php" name="workshops" id="workshop" method="post">
+							<input type="hidden" name="price" value="50"/>
+							<fieldset>
+								<legend class="visuallyhidden">Personlige oplysninger</legend>
 								<label>
-									<span>Barnets navn</span>
-									<input type="text" name="workshop[0][0][childname]" placeholder="Dit barns navn" />
+									<span>Email adresse</span>
+									<input type="text" name="email" placeholder="ditnavn@mail.dk" />
 								</label>
 								<label>
-									<span>Barnets alder</span>
-									<select name="workshop[0][0][childage]">
-										<option value="5">5 år</option> 
-										<option value="6">6 år</option>
-										<option value="7">7 år</option>
-									</select>
+									<span>Mobilnummer</span>
+									<input type="text" name="phone" placeholder="12345678" />
 								</label>
-								<a href="#" class="add-person">tilmeld flere børn</a>
+							</fieldset>
+
+							<fieldset>
+								<legend class="visuallyhidden">Vælg workshop</legend>
+								<label>
+									<input type="checkbox" name="checkbox[0]" /> <!-- checked="checked"-->
+									<span>Vælg workshop 1</span>
+								</label>
+								<div class="form-group" id="0">
+									<label>
+										<span>Barnets navn</span>
+										<input type="text" name="workshop[0][0][childname]" placeholder="Dit barns navn" />
+									</label>
+									<label>
+										<span>Barnets alder</span>
+										<select name="workshop[0][0][childage]">
+											<option value="5">5 år</option> 
+											<option value="6">6 år</option>
+											<option value="7">7 år</option>
+										</select>
+									</label>
+									<a href="#" class="add-person">tilmeld flere børn</a>
+								</div>
+
+								<label>
+									<input type="checkbox" name="checkbox[1]" />
+									<span>Vælg workshop 2</span>
+								</label>
+								<div class="form-group" id="1">
+									<label>
+										<span>Barnets navn</span>
+										<input type="text" name="workshop[1][0][childname]" placeholder="Dit barns navn" />
+									</label>
+									<label>
+										<span>Barnets alder</span>
+										<select name="workshop[1][0][childage]">
+											<option value="8">8 år</option> 
+											<option value="9">9 år</option>
+											<option value="10">10 år</option>
+										</select>
+									</label>
+									<a href="#" class="add-person">tilmeld flere børn</a>
+								</div>
+							</fieldset>
+							<fieldset>
+								<legend class="visuallyhidden">Personlig besked</legend>
+								<label>
+									<span>Skriv eventuelt en besked hvis du vil tilføje noget...</span>
+									<textarea name="message" placeholder="Personlig besked"></textarea>
+								</label>
+							</fieldset>
+							<div class="buttons">
+								<input type="submit" value="Tilmeld" />
 							</div>
+						</form>
 
-							<label>
-								<input type="checkbox" name="checkbox[1]" />
-								<span>Vælg workshop 2</span>
-							</label>
-							<div class="form-group" id="1">
-								<label>
-									<span>Barnets navn</span>
-									<input type="text" name="workshop[1][0][childname]" placeholder="Dit barns navn" />
-								</label>
-								<label>
-									<span>Barnets alder</span>
-									<select name="workshop[1][0][childage]">
-										<option value="8">8 år</option> 
-										<option value="9">9 år</option>
-										<option value="10">10 år</option>
-									</select>
-								</label>
-								<a href="#" class="add-person">tilmeld flere børn</a>
-							</div>
-						</fieldset>
-						<fieldset>
-							<legend class="visuallyhidden">Personlig besked</legend>
-							<label>
-								<span>Skriv eventuelt en besked hvis du vil tilføje noget...</span>
-								<textarea name="message" placeholder="Personlig besked"></textarea>
-							</label>
-						</fieldset>
-						<div class="buttons">
-							<input type="submit" value="Tilmeld" />
+						<div class="thank-you hide">
+							<h2>Tak for tilmeldingen</h2>
+							<p>Bla bla</p>
 						</div>
-					</form>
 
-					<div class="thank-you hide">
-						<h2>Tak for tilmeldingen</h2>
-						<p>Bla bla</p>
-					</div>
+					</section>
 
-				</section>
+				</div>
 
-                <aside>
+                <aside class="clearfix">
                     <h3 role="heading" aria-level="3">Sassi Bischoff</h3>
-                    <p><!--img src="static/img/_sassi-bischoff-sm.jpg" alt="Sassi Bischoff" width="200" height="297" /-->Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
+                    <p><img src="static/img/sassi-bischoff-sm.jpg" alt="Sassi Bischoff" width="200" height="297" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
                 </aside>
 
             </div> <!-- #main -->
